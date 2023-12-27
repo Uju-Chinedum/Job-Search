b4155b604b5fb3114922f95e981f702d356c659d
Savadow <ujuchimaraoke@gmail.com>
2023-12-27 21:26:39 +0100

    feat: called notFound, errorHandler middlewares and auth route

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 require("dotenv").config();
 require("express-async-errors");
 const express = require("express");
+
 const connectDB = require("./db/connect");
+const notFound = require("./middleware/notFound");
+const errorHandler = require("./middleware/errorHandler");
+const authRouter = require("./routes/authRoute");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+
+app.use("/api/v1/auth", authRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const start = async () => {
   try {
